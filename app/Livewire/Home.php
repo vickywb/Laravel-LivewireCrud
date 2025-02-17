4a510cf8262cbe59d260be07a9e99dd6f5bbde54
vicky wibisono <wibivicky@gmail.com>
2025-02-17 16:49:34 +0700

    update home

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -8,11 +8,17 @@ use Livewire\WithPagination;
 
 class Home extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.home',
         [
-            'articles' => Article::with('user')->latest()->get()
+            'articles' => Article::with('user')
+                ->latest()
+                ->take(3)
+                ->get()
         ])
         ->title('Home');
     }
